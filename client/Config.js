dc3812a089b0063479e37920abe6eba0dbc5c918
Gregory Wickstrom <gwick001@ucr.edu>
2017-03-29 18:03:47 -0700

    this is the first

---
+++ client/Config.js
@@ -0,0 +1,24 @@
+var app = angular.module("app", ['ngRoute']);
+
+app.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: '/partials/main.ejs',
+		controller: 'userController'
+	})
+	.when('/dashboard', {
+		templateUrl: '/partials/dashboard.ejs',
+		controller: 'dashboardController'
+	})
+	.when('/topic/:id', {
+		templateUrl: '/partials/topic.ejs',
+		controller: 'topicController'
+	})
+	.when('/user/:id', {
+		templateUrl: '/partials/user.ejs',
+		controller: 'userController'
+	})
+	.otherwise({
+		redirectTo: '/'
+	})
+})
